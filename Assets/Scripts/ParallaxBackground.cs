42c0efe5c50caa34113ad327de23353b67d51659
hyyt <3295675539@qq.com>
2024-10-17 12:07:10 +0800

    add-parallax

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    private GameObject cam;
+    [SerializeField] private float parallaxEffect;
+
+    private float xPosition;
+    private float length;
+    void Start()
+    {
+        cam = GameObject.Find("Virtual Camera");
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        xPosition = transform.position.x;
+    }
+
+    void Update()
+    {
+        float disTanceMoved = cam.transform.position.x * (1 - parallaxEffect);
+        float distanceToMove = cam .transform.position.x * parallaxEffect;
+
+        transform.position = new Vector3(xPosition + distanceToMove, transform.position.y);
+
+        if (disTanceMoved > xPosition + length) xPosition = xPosition + length;
+        else if(disTanceMoved < xPosition - length) xPosition = xPosition - length;
+    }
+}
